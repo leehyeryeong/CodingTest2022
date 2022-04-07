2f4838a40cf85c9ff86f0c13d98fd9c21d2312ab
leehyeryeong <84839619+leehyeryeong@users.noreply.github.com>
2022-04-07 11:12:20 +0900

    Update char1.c

--- char1.c
+++ char1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	char zero = '0';
+	/*char zero = '0';
 	int n = zero - 48;
 	printf("%c\n", zero);
-	printf("%d\n", n);
+	printf("%d\n", n);*/
+	char ch = '9';
+	int number = ch - 48;
+	int n = 9;
+
+	printf("문자: %c, 수: %d, 수: %d\n", ch, number, n);
 
 	return 0;
 }
