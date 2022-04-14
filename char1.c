ce74f80dc3425413a7e7c391a342bdfdbbeab0a6
이혜령 <84839619+leehyeryeong@users.noreply.github.com>
2022-04-15 00:47:30 +0900

    Add files via upload

--- char1.c
+++ char1.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	/*char zero = '0';
-	int n = zero - 48;
-	printf("%c\n", zero);
-	printf("%d\n", n);*/
-	char ch = '9';
-	int number = ch - 48;
-	int n = 9;
+	//char zero = '0';
+	//int n = zero - 48;
+	//printf("%c\n", zero);
+	//printf("%d\n", n);
+	//char ch = '9';
+	//int number = ch - 48;
+	//int n = 9;
+	//printf("¹®ÀÚ: %c, ¼ö: %d, ¼ö: %d\n", ch, number, n);
 
-	printf("ë¬¸ì: %c, ìˆ˜: %d, ìˆ˜: %d\n", ch, number, n);
+	char s1[] = "COS PRO";
+	char s2[10] = { 'k', 'o', 'r', 'e', 'a' };
+
+	//s1 = "mirim high"; //¹®ÀÚ¿­ ¹è¿­ ÀÌ¸§¿¡ ´ëÀÔ¿¬»ê ºÒ°¡
+
+	printf("%s %d\n", s1, sizeof(s1));
+	printf("%s %d\n", s2, sizeof(s2));
 
 	return 0;
 }
